97a37ea60694646d312e56a6dee4888908d0c491
Mert <81412246+meratad@users.noreply.github.com>
2023-09-25 22:02:34 +0300

    Update odev_10.sql

--- odev_10.sql
+++ odev_10.sql
@@ -1,14 +1,14 @@
 1. city tablosu ile country tablosunda bulunan şehir (city) ve ülke (country) isimlerini birlikte görebileceğimiz LEFT JOIN sorgusunu yazınız.
 
-SELECT city, country FROM city
-LEFT JOIN country ON country.country_id = country.country_id;
+SELECT city.country_id,country.country_id,city.city, country.country FROM city 
+LEFT JOIN country ON country.country_id = city.country_id;
 
 2. customer tablosu ile payment tablosunda bulunan payment_id ile customer tablosundaki first_name ve last_name isimlerini birlikte görebileceğimiz RIGHT JOIN sorgusunu yazınız.
 
-SELECT payment_id, first_name, last_name FROM customer
-RIGHT JOIN payment ON payment.payment_id = payment.payment_id;
+SELECT customer.first_name, customer.last_name, payment.payment_id FROM customer 
+RIGHT JOIN payment ON payment.customer_id = customer.customer_id;
 
 3. customer tablosu ile rental tablosunda bulunan rental_id ile customer tablosundaki first_name ve last_name isimlerini birlikte görebileceğimiz FULL JOIN sorgusunu yazınız.
 
-SELECT rental_id, first_name, last_name FROM customer
-FULL JOIN rental ON rental.rental_id = rental.rental_id;
+SELECT customer.first_name, customer.last_name, rental.rental_id  FROM customer
+FULL JOIN rental ON customer.customer_id = rental.customer_id;
